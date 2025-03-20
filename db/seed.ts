88cdabd3347f92b6182bb465d0d09213a9baecbc
Alonso Massa <alonso@MacBook-Pro-de-Alonso.local>
2025-03-19 19:56:16 -0500

    lodash debounce

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,5 @@
-import { Clients, db } from 'astro:db';
+import { getCollection } from 'astro:content';
+import { Clients, db, Posts } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -12,4 +13,13 @@ export default async function seed() {
 		{ id: 4, name: "Maria",age: 29, isActive: true },
 	  ]);
 	
+	  const posts = await getCollection('blog');
+
+	  await db.insert(Posts).values(
+		posts.map((post) => ({
+		  id: post.id,
+		  title: post.data.title,
+		  likes: Math.round(Math.random() * 100),
+		}))
+	  );
 }
--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -4,28 +4,33 @@ import { getEntry } from "astro:content";
 export const prerender = false;
 
 //si  deseo puedo poner las cookies entre los datos
-export const GET: APIRoute = async ({params, request}) => {
-    const {slug} = params;
+export const GET: APIRoute = async ({ params, request }) => {
+    const { slug } = params;
     //slug -> toma el nombre de la carpeta como parametro
-    const post = await getEntry('blog', slug as any );
+    const post = await getEntry('blog', slug as any);
 
     if (!post) {
         return new Response(
-            JSON.stringify({msg:`No se encuentra ${slug}`}), 
+            JSON.stringify({ msg: `No se encuentra ${slug}` }),
             { status: 404 }
         );
     }
 
     return new Response(
-        JSON.stringify(post), 
-        { status: 200 }
+        JSON.stringify(post),
+        {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
     );
 };
 
-export const POST: APIRoute = async ({params, request}) => {
+export const POST: APIRoute = async ({ params, request }) => {
     const body = await request.json();
-    
-    return new Response(JSON.stringify(body), { 
+
+    return new Response(JSON.stringify(body), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
@@ -33,13 +38,13 @@ export const POST: APIRoute = async ({params, request}) => {
     });
 }
 
-export const PUT: APIRoute = async ({params, request}) => {
+export const PUT: APIRoute = async ({ params, request }) => {
     const body = await request.json();
-    
+
     return new Response(JSON.stringify(({
         method: 'PUT',
         ...body,
-    })), { 
+    })), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
@@ -47,13 +52,13 @@ export const PUT: APIRoute = async ({params, request}) => {
     });
 }
 
-export const PATCH: APIRoute = async ({params, request}) => {
+export const PATCH: APIRoute = async ({ params, request }) => {
     const body = await request.json();
-    
+
     return new Response(JSON.stringify(({
         method: 'PATCH',
         ...body,
-    })), { 
+    })), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
@@ -61,13 +66,13 @@ export const PATCH: APIRoute = async ({params, request}) => {
     });
 }
 
-export const DELETE: APIRoute = async ({params, request}) => {
-    const {slug} = params;
-    
+export const DELETE: APIRoute = async ({ params, request }) => {
+    const { slug } = params;
+
     return new Response(JSON.stringify(({
         method: 'DELETE',
         slug: slug
-    })), { 
+    })), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
